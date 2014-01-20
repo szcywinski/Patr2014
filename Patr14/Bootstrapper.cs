dd3095e355429caa30d144b6e320e7597d2cde3b
Szymon Cywinski <sz.cywinski@gmail.com>
2014-01-20 23:57:18 +0100

    initial commit

---
+++ Patr14/Bootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using Caliburn.Micro;
+using Patr14.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaliburnMicro
+{
+    public class Bootstrapper : PhoneBootstrapper
+    {
+        PhoneContainer container;
+
+        protected override void Configure()
+        {
+            if (Execute.InDesignMode)
+                return;
+
+            container = new PhoneContainer();
+
+            container.RegisterPhoneServices(RootFrame);
+            container.PerRequest<MainPageViewModel>();
+
+            AddCustomConventions();
+        }
+
+        static void AddCustomConventions()
+        {
+            //ellided 
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+    }
+}
---
+++ Patr14/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Patr14.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            MessageBox.Show("I'm the MainView!");
+        }
+    }
+}
