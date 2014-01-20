5d0a1e8dc1a3a0df4b165c6cd8f4f2f225696160
Szymon Cywinski <sz.cywinski@gmail.com>
2014-01-21 00:59:46 +0100

    listbox handling

---
+++ Patr14/Model/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patr14
+{
+    [DataContract]
+    public class Student : INotifyPropertyChanged
+    {
+        private String firstName = "";
+        private String lastName = "";
+        private Decimal grade = 2m;
+
+
+        [DataMember]
+        public String FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; NotifyPropertyChanged("FirstName"); }
+        }
+
+        [DataMember]
+        public String LastName
+        {
+            get { return lastName; }
+            set { lastName = value; NotifyPropertyChanged("LastName"); }
+        }
+
+        [DataMember]
+        public Decimal Grade
+        {
+            get { return grade; }
+            set { grade = value; NotifyPropertyChanged("Grade"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- Patr14/ViewModels/MainPageViewModel.cs
+++ Patr14/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,20 +12,55 @@ namespace Patr14.ViewModels
     public class MainPageViewModel : PropertyChangedBase
     {
         private decimal averageGrade;
+        private ObservableCollection<Student> students;
+        private Student selectedStudent;
+       
+        
+        public MainPageViewModel()
+        {
+            AverageGrade = 3.456m;
+            Students = new BindableCollection<Student>
+            {
+                new Student(){FirstName="Janusz",LastName="Pień",Grade=3.0m},
+                new Student(){FirstName="Marian",LastName="Kutacz",Grade=3.0m}
+            };
+        }
+
+        public void AddStudent()
+        {
+            MessageBox.Show("Item added");
+        }
+
+        public Student SelectedStudent
+        {
+            get { return selectedStudent; }
+            set
+            {
+                selectedStudent = value;
+                NotifyOfPropertyChange(() => SelectedStudent);
+                MessageBox.Show(value.Grade.ToString());
+            }
+        }
 
         public decimal AverageGrade
         {
             get { return averageGrade; }
-            set 
-            { 
+            set
+            {
                 averageGrade = value;
                 NotifyOfPropertyChange(() => AverageGrade);
             }
         }
 
-        public MainPageViewModel()
+        public ObservableCollection<Student> Students
         {
-            AverageGrade = 3.456m;
+            get { return students; }
+            set
+            {
+                students = value;
+                NotifyOfPropertyChange(() => Students);
+            }
+
         }
     }
 }
